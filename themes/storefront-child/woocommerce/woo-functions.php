706335eea8e9196ea992c64067cf6e99ab2c28e3
root <root@sgoodrich12763>
2018-03-07 13:01:04 +0000

    Backup

---
+++ themes/storefront-child/woocommerce/woo-functions.php
@@ -0,0 +1,12 @@
+<?php
+			 
+//	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
+//	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10);
+//	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
+//	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
+//	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30);
+//	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+//	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50);
+//	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_product_data', 60);			 
+			 
+?>
