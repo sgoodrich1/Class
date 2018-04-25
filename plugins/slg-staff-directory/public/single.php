0bf0bdd1330ff8a98fc6161c2fbc39599c591ab8
root <root@sgoodrich12763>
2018-04-25 13:01:01 +0000

    Backup

--- plugins/slg-staff-directory/public/single.php
+++
@@ -1,9 +0,0 @@
-<?php
-get_header();
-?>
-this is the plugin single template 
-
-<b>Page Title:</b><?php the_title(sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>'); ?><br>
-
-<?php the_post_thumbnail(); ?><br>
-<b>Page Content:</b><?php the_content(); ?><br>
